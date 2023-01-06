dd02afd1840ca65684334b8c2c4b59975a48b66d
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-06 23:36:24 +0530

    [amol]- Check Leap Year

---
+++ BsicCoreProgramnig/BsicCoreProgramnig/LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BsicCoreProgramnig
+{
+    public class LeapYear
+    {
+        public void FindLeap()
+        {
+            Console.WriteLine("Enter a Year");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            if (Year % 4 == 0)
+            {
+                Console.WriteLine("It is a Leap Year");
+            }
+            else if (Year % 100 == 0)
+            {
+                Console.WriteLine("It is not a leap Year");
+            }
+            else if (Year % 400 == 0)
+            {
+                Console.WriteLine("It is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("it is a not leap year");
+            }
+        }
+    }
+}
--- BsicCoreProgramnig/BsicCoreProgramnig/Program.cs
+++ BsicCoreProgramnig/BsicCoreProgramnig/Program.cs
@@ -10,13 +10,17 @@ namespace BsicCoreProgramnig
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.Flip Coin ");
+            Console.WriteLine("1.Flip Coin\n2.Leap Year ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.CheckHeadTail();
+                    break;
+                case 2:
+                    LeapYear leap = new LeapYear();
+                    leap.FindLeap();
                     break;
                 default:
                     Console.WriteLine("Please enter valid choice");
