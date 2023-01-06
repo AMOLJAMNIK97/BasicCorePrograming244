0bf9d7180f4f355b75d47f4e5810986cd0fb25ed
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-06 23:23:09 +0530

    [amol]- Flip Coin and print percentage of Heads and Tails

--- BsicCoreProgramnig/BsicCoreProgramnig/Program.cs
+++ BsicCoreProgramnig/BsicCoreProgramnig/Program.cs
@@ -10,25 +10,13 @@ namespace BsicCoreProgramnig
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.Flip Coin\n2.Check Leap Year\n3. Check power of two\n4. Harmonic Number");
+            Console.WriteLine("1.Flip Coin ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    FlipCoin flipcoin = new FlipCoin();
-                    flipcoin.CheckHeadTail();
-                    break;
-                case 2:
-                    LeapYear leapyear = new LeapYear();
-                    leapyear.FindLeap();
-                    break;
-                case 3:
-                    PowerOfTwo power = new PowerOfTwo();
-                    power.FindPower();
-                    break;
-                case 4:
-                    Harmonic harmonic = new Harmonic();
-                    harmonic.FindHarmoic();
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.CheckHeadTail();
                     break;
                 default:
                     Console.WriteLine("Please enter valid choice");
