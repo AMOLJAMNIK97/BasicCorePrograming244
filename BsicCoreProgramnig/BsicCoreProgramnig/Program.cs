74f089c92dfc6e7d9397f2ea710d7e3b29089e34
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-06 23:15:17 +0530

    [amol]- Basic Core Programing

---
+++ BsicCoreProgramnig/BsicCoreProgramnig/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BsicCoreProgramnig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Flip Coin\n2.Check Leap Year\n3. Check power of two\n4. Harmonic Number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipcoin = new FlipCoin();
+                    flipcoin.CheckHeadTail();
+                    break;
+                case 2:
+                    LeapYear leapyear = new LeapYear();
+                    leapyear.FindLeap();
+                    break;
+                case 3:
+                    PowerOfTwo power = new PowerOfTwo();
+                    power.FindPower();
+                    break;
+                case 4:
+                    Harmonic harmonic = new Harmonic();
+                    harmonic.FindHarmoic();
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid choice");
+                    break;
+            }
+        }
+    }
+}
