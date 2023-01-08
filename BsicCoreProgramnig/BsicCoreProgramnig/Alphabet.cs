04a20fc2f17317e3801022995d3f94fe649e2c0e
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-08 23:16:47 +0530

    [amol]- Check Whether an Alphabet is Vowel or Consonant

---
+++ BsicCoreProgramnig/BsicCoreProgramnig/Alphabet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BsicCoreProgramnig
+{
+    public class Alphabet
+    {
+        public void CheckConsonantOrVowel()
+        {
+            char input;
+            Console.WriteLine("Enter a Character");
+            input = Convert.ToChar(Console.ReadLine());
+            if (input == 'a' || input == 'A' || input == 'e' || input == 'E' || input == 'i' || input == 'I' || input == 'o' || input == 'O' || input == 'u' || input == 'U')
+            {
+                Console.WriteLine(input + " is a Vowel");
+            }
+            else
+            {
+                Console.WriteLine(input + " is a Consonant");
+            }
+
+        }
+    }
+}
--- BsicCoreProgramnig/BsicCoreProgramnig/Program.cs
+++ BsicCoreProgramnig/BsicCoreProgramnig/Program.cs
@@ -10,7 +10,7 @@ namespace BsicCoreProgramnig
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of two\n4.Harmonic\n5. Factors\n6.Sawp Two Number\n7. Check Number Even or Odd\n8.Quotient Remainder\n9.Largest three Number");
+            Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of two\n4.Harmonic\n5. Factors\n6.Sawp Two Number\n7. Check Number Even or Odd\n8.Quotient Remainder\n9.Largest three Number\n10.Alphabet is Vowel or Consonant");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -49,6 +49,10 @@ namespace BsicCoreProgramnig
                 case 9:
                     LargestNumber largest = new LargestNumber();
                     largest.FindThreeNumber();
+                    break;
+                case 10:
+                    Alphabet check = new Alphabet();
+                    check.CheckConsonantOrVowel();
                     break;
                 default:
                     Console.WriteLine("Please enter valid choice");
